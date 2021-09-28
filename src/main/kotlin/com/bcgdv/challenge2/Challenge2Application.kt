be132a14ae4798fbf49af636332ddb52ea81bd01
Nhan Tran <thcnhan@gmail.com>
2021-09-28 19:57:46 +0800

    First commit.

---
+++ src/main/kotlin/com/bcgdv/challenge2/Challenge2Application.kt
@@ -0,0 +1,11 @@
+package com.bcgdv.challenge2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Challenge2Application
+
+fun main(args: Array<String>) {
+	runApplication<Challenge2Application>(*args)
+}
